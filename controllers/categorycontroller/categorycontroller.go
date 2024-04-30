050f76dd36bcb6326b93e4bdd0c1d38ac36e902c
akmlrnyn <akmalranyan@gmail.com>
2024-04-30 11:34:00 +0700

    made update category

--- controllers/categorycontroller/categorycontroller.go
+++ controllers/categorycontroller/categorycontroller.go
@@ -79,7 +79,25 @@ func Edit(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//If requests the update method
-	
+	if r.Method == "POST" {
+		var category entities.Category
+
+		idString := r.FormValue("id")
+		id, err := strconv.Atoi(idString)
+
+		if err != nil {
+			panic(err)
+		}
+
+		category.Name = r.FormValue("name")
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Update(id, category); !ok {
+			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+		}
+
+		http.Redirect(w, r, r.Header.Get("/categories"), http.StatusSeeOther)
+	}
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
