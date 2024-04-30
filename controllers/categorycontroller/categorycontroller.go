3c33b8aaae6fcd2ec0bda2501d18596391159e14
akmlrnyn <akmalranyan@gmail.com>
2024-04-30 08:55:31 +0700

    made store category Controller, Model, and View

--- controllers/categorycontroller/categorycontroller.go
+++ controllers/categorycontroller/categorycontroller.go
@@ -1,9 +1,11 @@
 package categorycontroller
 
 import (
+	"crud-go/entities"
 	"crud-go/models/categorymodel"
 	"net/http"
 	"text/template"
+	"time"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -22,7 +24,33 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func Add(w http.ResponseWriter, r *http.Request) {
-	
+	//If requests the add category page
+	if r.Method == "GET" {
+		temp, err := template.ParseFiles("views/category/create.html")
+
+		if err != nil {
+			panic(err)
+		}
+
+		temp.Execute(w, nil)
+	}
+
+	//If requests the store method
+	if r.Method == "POST" {
+		var category entities.Category
+
+		category.Name = r.FormValue("name")
+		category.CreatedAt = time.Now()
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Create(category); !ok {
+			temp, _ := template.ParseFiles("views/category/create.html")
+
+			temp.Execute(w, nil)
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
+	}
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
